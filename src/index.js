b7248b74d238fa258c9dae9a59948decfe505775
LemniscataOK <kolibanoksana@gmail.com>
2024-02-29 17:29:42 +0200

    Updated footer

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-let apiKey = "act34503b8f3f488c5379b038o28b667";
   let city = searchInputElement.value;
+
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(updateWeather);
-}
 
-function updateWeather(response) {  
-let cityElement = document.querySelector("#current-city");
-let currentCity = response.data.city;
- cityElement.innerHTML = currentCity;
- let currentTemperature = document.querySelector(
-    "span.current-temperature-value"
-  ); 
-
-  currentTemperature.innerHTML = Math.round(response.data.temperature.current);
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let day = date.getDay();
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+
   if (hours < 10) {
     hours = `0${hours}`;
   }
+
   let days = [
     "Sunday",
     "Monday",
@@ -35,8 +37,9 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday",
+    "Saturday"
   ];
+
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}`;
 }
@@ -46,4 +49,5 @@ searchForm.addEventListener("submit", search);
 
 let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
+
 currentDateELement.innerHTML = formatDate(currentDate);
